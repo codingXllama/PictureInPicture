4b78988cf03498645ef872b32c4980d721b583f8
codingxllama <osamaalchalabi2@gmail.com>
2023-10-02 22:20:07 -0400

    x

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let startBtn = startBtn.getElementById("startBtn");
-startBtn.addEventListener("click", () => {
-  alert("hi");
-});
+const videoElement = document.getElementById("video");
+const button = document.getElementById("startBtn");
+
+//prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+  try {
+    something;
+  } catch (error) {
+    // catch error here
+    console.log("Whoops error here ", error);
+  }
+}
+
+// On Load
+selectMediaStream();
